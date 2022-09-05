99d5e30a988a18565fb943e68614757fbeb81b7c
VadimCurca <vadim.curca14@gmail.com>
2022-09-05 21:39:30 +0300

    add dummy main

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from src.model import *
+
+torch.manual_seed(42)
+torch.backends.cudnn.deterministic = True
 
 if __name__ == '__main__':
     print("Hi pycharm")
+
+    net = shufflenet_g8_w1()
+
+    X = torch.rand(1, 3, 224, 224)
+    X = net(X)
+    print(net.__class__.__name__, 'output shape:\t', X.shape)
