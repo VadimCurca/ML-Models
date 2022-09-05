90f9348f2615fee8a452170699871dbcbf9f190b
VadimCurca <vadim.curca14@gmail.com>
2022-09-05 21:48:05 +0300

    MaskRCNN: remove unused files

--- PyTorch-Simple-MaskRCNN/pytorch_mask_rcnn/__init__.py
+++ PyTorch-Simple-MaskRCNN/pytorch_mask_rcnn/__init__.py
@@ -2,9 +2,9 @@ from .model import maskrcnn_resnet50_fpn
 from .datasets import *
 from .engine import train_one_epoch, evaluate
 from .utils import *
-from .gpu import *
 
 try:
     from .visualizer import *
 except ImportError:
     pass
+
